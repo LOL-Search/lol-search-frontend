c2f50a13f0c1525d3a7e0f940e493e07c6c0ae46
rabbithole333 <theworstvalentine@gmail.com>
2024-12-19 13:02:55 +0900

    "#4 Feat: 메뉴 및 로그인, 회원가입 ui 구현"

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,16 @@ import WriteForm from "./pages/board/WriteForm";
 import { ConfigProvider } from "antd";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PostList from "./pages/Board/PostList";
+import ChatList from "./pages/Chat/ChatList";
+import ChatAdd from "./pages/Chat/ChatAdd";
+import ChatRoom from "./pages/Chat/ChatRoom";
+import ViewPost from "./pages/Board/ViewPost";
 import UserStats from "./pages/userStat/UserStat";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+
+
+
 
 function App() {
   return (
@@ -23,10 +30,15 @@ function App() {
         >
           <BrowserRouter>
             <Routes>
+              <Route path="/" element={<ChatList />} />
+              <Route path="/chat/add" element={<ChatAdd />} />
+              <Route path="/chat/room" element={<ChatRoom />} />
               <Route path="/board/write" element={<WriteForm />} />
               <Route path="/board/list" element={<PostList />} />
               <Route path="/board/view" element={<ViewPost />} />
               <Route path="/user/search" element={<UserStats />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
             </Routes>
           </BrowserRouter>
         </ConfigProvider>
